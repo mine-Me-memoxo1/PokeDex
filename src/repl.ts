5503014c1bb35dc019eb0c6e07e403aa14befe67
labhi10.20 <labhi10.20@proton.me>
2026-04-22 12:48:40 -0400

    Added StartREPL method and is called in main functions. Adjusted config in tsconfig.json and added property type to prevent error when using the CreateInterface

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,26 @@
+import { createInterface } from "readline";
+
 export function cleanInput(input: string): string[] {
 	return input.trim().toLowerCase().split(/\s+/);
+}
+
+
+export function startREPL() {
+	const rl = createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		prompt: `PokeDex > `
+	});
+
+	rl.prompt();
+	
+	rl.on("line", (line) => {
+		let words = cleanInput(line)
+		if (words.length === 0) {
+			rl.prompt();
+			return;
+		}
+		console.log(`Your command was: ${words[0]}`);
+		rl.prompt();
+	})
 }
